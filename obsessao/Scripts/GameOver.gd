88bb9b7ebb1b07e922cf3c86651ffb34edd717af
fiwon123 <felipeinoue@yahoo.com.br>
2019-06-12 11:44:36 -0300

    Put music game over

--- obsessao/Scripts/GameOver.gd
+++ obsessao/Scripts/GameOver.gd
@@ -1,5 +1,11 @@
 extends Control
 
+var audio_stream = "res://SFX/Screen/game_over.wav"
+
+func _ready():
+	Global.audio.stream = load(audio_stream)
+	Global.audio.play()
+
 func _on_ButtonRetry_pressed():
 	get_tree().change_scene(Global.LEVEL1)
 
